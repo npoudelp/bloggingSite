e7258de1383c7782648002dc511ce44c6169d770
doctorCrazy <npoudelp@gmail.com>
2021-06-23 10:19:38 +0545

    Route for home and post added

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const url = "mongodb://127.0.0.1/blogSiteDB";
+
+mongoose.connect(url, { useNewUrlParser: true }).then(() => {
+  console.log("Database Connected....");
+});
+
+const app = express();
+
+const cors = require("cors");
+
+app.use(express.json());
+
+app.use(cors());
+
+const homeRoute = require("./routes/home");
+app.use("/", homeRoute);
+
+const postRoute = require("./routes/post");
+app.use("/post", postRoute);
+
+app.listen(8090, () => {
+  console.log("Server Started....");
+});
