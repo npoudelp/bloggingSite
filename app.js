969594da45d76e149c9b2d5fca93075285d12152
doctorCrazy <npoudelp@gmail.com>
2021-06-24 10:21:48 +0545

    Contact route added

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use("/", homeRoute);
 const postRoute = require("./routes/post");
 app.use("/post", postRoute);
 
+const contactRoute = require("./routes/contact");
+app.use("/contact", contactRoute);
+
 app.listen(8090, () => {
   console.log("Server Started....");
 });
