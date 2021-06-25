a9b4d31b4f8196ae5daf155e63ac41a59656762d
doctorCrazy <npoudelp@gmail.com>
2021-06-25 09:49:06 +0545

    Individual post get on click made

--- clientUI/js/main.js
+++ clientUI/js/main.js
@@ -18,31 +18,34 @@ function appendPost(param) {
     let date = param[i].date;
     let id = param[i]._id;
     let tag = param[i].tag;
-    //console.log(id);
+    console.log("Logged before: " + id);
+
     $("#postDisplay").prepend(
-      "<div class='col-lg-4 col-md-12 mb-4'><div class='card shadow mb-5'><div class='bg-image hover-overlay ripple' data-mdb-ripple-color='light'><img src='./images/demo.jpg' class='img-fluid' /><a href='#'><div class='mask' style='background-color: rgba(251, 251, 251, 0.15);'></div></a></div><div class='card-body'><h5 class='card-title'>" +
+      "<div class='col-lg-4 col-md-12 mb-4'><div class='card shadow mb-5'><div class='bg-image hover-overlay ripple' data-mdb-ripple-color='light'><img src='./images/demo.jpg' class='img-fluid' /><a href='#'><div class='mask' style='background-color: rgba(251, 251, 251, 0.15);'></div></a></div><div class='card-body'><h2 class='card-title'>" +
         title +
         "</h5> <p class='card-text'>" +
         post +
-        "</p><button onclick='readMore(" +
+        "</p><a onclick=readMore('" +
         id +
-        ")' class='btn btn-primary float-start shadow mb-5'>Read</button><span> Tag:" +
+        "') class='btn btn-primary float-start shadow mb-5'>Read</a><span class='postTag'> By: " +
+        author +
+        " on " +
         tag +
-        "</span class='postTag'></div></div></div>"
+        "</span></div></div></div>"
     );
   });
 }
-function readMore(param) {
-    // const url = "http://127.0.0.1:8090/post/"+param;
-    console.log(param);
-    $.ajax({
-        type: "GET",
-        url: "http://127.0.0.1:8090/post/"+param+"",
-        dataType: 'json',
-        contentType: 'application/json',
-        success: (data) => {
-            //displayPost(data);
-            console.log(data);
-        }
-    })
-  }
+function readMore(postId) {
+  console.log(postId);
+  $.ajax({
+    type: "GET",
+    url: "http://127.0.0.1:8090/post/" + postId,
+    dataType: "json",
+    contentType: "application/json",
+    success: (data) => {
+      console.log(data);      
+    },
+  });
+}
+
+
